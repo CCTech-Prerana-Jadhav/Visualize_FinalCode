bf58eaa788171d31c28e40cfb660c1b39e525229
cctechyashdoshi <yash.doshi@cctech.co.in>
2024-11-04 09:10:48 +0530

    Update Visualizer.h

--- Visualizer/headers/Visualizer.h
+++ Visualizer/headers/Visualizer.h
@@ -24,6 +24,8 @@ private slots:
 private:
     void setupUi();
     OpenGlWidget::Data convertTrianglulationToGraphicsObject(const Triangulation& inTriangulation);
+    Triangulation readFile(const QString& filePath);
+    void writeFile(const QString& filePath, const Triangulation& tri);
 
 private:
     Triangulation triangulation;
@@ -39,8 +41,6 @@ private:
     GraphicsSynchronizer* graphicsSynchronizer;
 
     QVector<GLfloat> vertices;
-
-
 
     // translation data
     QString inputFilePath;
