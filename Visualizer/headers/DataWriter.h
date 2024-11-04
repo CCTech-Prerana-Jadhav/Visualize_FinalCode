85082578055f2cb976f2010397736238b94c20fc
Manav Gupta <manav.gupta@cctech.co.in>
2024-11-04 10:23:17 +0530

    final

--- Visualizer/headers/DataWriter.h
+++ Visualizer/headers/DataWriter.h
@@ -7,6 +7,7 @@ class DataWriter : public Writer
 public:
     void Write(const std::string& filename, const Triangulation& triangulation);
 
+
 private:
     std::string fomulateText(Triangulation triangulation, Point point);
 };
