eb1d41de4c3f790a9d7844c5d0657421bad076a5
cctechyashdoshi <yash.doshi@cctech.co.in>
2024-11-05 16:30:06 +0530

    Update main.cpp

--- Visualizer/main.cpp
+++ Visualizer/main.cpp
@@ -1,11 +1,15 @@
 #include "Visualizer.h"
 #include <QtWidgets/QApplication>
 
-int main(int argc, char *argv[])
+int main(int argc, char* argv[])
 {
     QApplication a(argc, argv);
+    QIcon appIcon("icon.png");
+    a.setWindowIcon(appIcon); 
+
     Visualizer w;
     w.show();
     w.setWindowState(Qt::WindowMaximized);
+
     return a.exec();
 }
