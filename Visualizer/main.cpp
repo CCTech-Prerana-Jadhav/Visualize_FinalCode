cfbe18f4a8338927a57fc7651d0b143dd9be6744
Yash Doshi <yash.doshi@cctech.co.in>
2024-11-06 13:58:53 +0530

    Fix incrrect path

--- Visualizer/main.cpp
+++ Visualizer/main.cpp
@@ -4,7 +4,7 @@
 int main(int argc, char* argv[])
 {
     QApplication a(argc, argv);
-    QIcon appIcon("icon.png");
+    QIcon appIcon("Resources/icon.png");
     a.setWindowIcon(appIcon); 
 
     Visualizer w;
