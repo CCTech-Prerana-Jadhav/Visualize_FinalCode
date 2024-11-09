2f9b8d7df5171c9f4931de107f9f1d997ccea66b
jaydeep-jogdand <jaydeep.jogdand@cctech.co.in>
2024-11-09 13:36:28 +0530

    final

---
+++ Transformation/headers/Transformation.h
@@ -0,0 +1,13 @@
+#pragma once
+#include "Triangulation.h"
+#include "Matrix4x4.h"
+namespace Transformation
+{
+	using namespace Geometry;
+	class Transformation
+	{
+	public:
+		bool operator()(double a, double b) const;
+		Triangulation scaling(Triangulation& triangulation, Geometry::Matrix4x4& matrix, double scalex = 2.0, double scaley = 2.0, double scalez = 1.0);
+	};
+}
